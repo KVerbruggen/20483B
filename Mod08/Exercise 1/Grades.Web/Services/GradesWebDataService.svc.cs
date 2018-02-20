87f670b7a60f5531cbaff4128601e4347045a7c5
Kevin Verbruggen <kevin.verbruggen@itvitaelearning.nl>
2018-02-20 15:00:01 +0100

    Module 8 Exercise 1: Creating a WCF Data Service for the SchoolGrades Database. Did all of the exercise, but can't get the service to open correctly in the browser. Had a lot of difficulty getting it to work, as for some reason I didn't have the option for a WCF Data Service, and DataServiceProtocolVersion V3 wasn't recognized, and some other problems.

---
+++ Mod08/Exercise 1/Grades.Web/Services/GradesWebDataService.svc.cs
@@ -0,0 +1,48 @@
+//------------------------------------------------------------------------------
+// <copyright file="WebDataService.svc.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Web;
+using System.Data.Services.Providers;
+using System.Data.Services.Client;
+using Grades.DataModel;
+
+namespace Grades.Web.Services
+{
+    public class GradesWebDataService : DataService<SchoolGradesDBEntities>
+    {
+        // This method is called only once to initialize service-wide policies.
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            // TODO: set rules to indicate which entity sets and service operations are visible, updatable, etc.
+            // Examples:
+            // config.SetEntitySetAccessRule("MyEntityset", EntitySetRights.AllRead);
+            // config.SetServiceOperationAccessRule("MyServiceOperation", ServiceOperationRights.All);
+            
+            config.SetEntitySetAccessRule("Grades", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Teachers", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Students", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Subjects", EntitySetRights.All);
+            config.SetEntitySetAccessRule("Users", EntitySetRights.All);
+            config.SetServiceOperationAccessRule("StudentInClass", ServiceOperationRights.AllRead);
+
+            config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V3;
+        }
+
+        [WebGet]
+        public IEnumerable<Student> StudentsInClass(string className)
+        {
+            return (from student in this.CurrentDataSource.Students
+                    where student.Teacher.Class == className
+                    select student);
+        }
+
+    }
+}
